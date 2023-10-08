c5c16826d862c3dd85e08a641216020ddaf4059f
Filipe Siqueira <filipedev040990@gmail.com>
2023-10-08 14:28:26 -0300

    feat: add CreateOrderController

--- src/application/controllers/orders/create-order.controller.ts
+++ src/application/controllers/orders/create-order.controller.ts
@@ -1,9 +1,17 @@
+import { ICreateOrderUseCase } from '@/ports'
 import { IController } from '@/ports/controllers/index.port'
+import { ServerError } from '@/shared/errors'
 import { success } from '@/shared/helpers/http.helper'
 import { HttpRequest, HttpResponse } from '@/shared/types/http.types'
 
 export class CreateOrderController implements IController {
+  constructor(private readonly createOrderUseCase: ICreateOrderUseCase) {}
   async execute (input: HttpRequest): Promise<HttpResponse> {
-    return success(200, {})
+    try {
+      const orderId = await this.createOrderUseCase.execute(input.body)
+      return success(201, { orderId })
+    } catch (error: any) {
+      throw new ServerError(error)
+    }
   }
 }
