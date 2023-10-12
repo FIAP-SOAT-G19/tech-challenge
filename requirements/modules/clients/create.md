9fa461a746819be8f67479266dc42739ab7fdb7e
Rafael Silva <rafaelsilvamap@gmail.com>
2023-10-12 14:53:19 -0300

    test: ensure CreateClientUseCase to validates required fields

--- requirements/modules/clients/create.md
+++ requirements/modules/clients/create.md
@@ -7,13 +7,14 @@
 3. ⛔ Retorna status 201 com id do cliente
 
 > ## Exceções
-1. ⛔ Retorna 400 se o nome do cliente não for fornecido
-2. ⛔ Retorna 400 se o email do cliente não for fornecido
-3. ⛔ Retorna 400 se o cpf do cliente não for fornecido
-4. ⛔ Retorna 400 se a senha do cliente não for fornecida
-5. ⛔ Retorna 400 se o email do cliente já existir
-6. ⛔ Retorna 400 se o cpf do cliente já existir
-5. ⛔ Retorna 500 se houver alguma falha na hora de salvar os dados
+1. ✅ Retorna 400 se o nome do cliente não for fornecido
+2. ✅ Retorna 400 se o email do cliente não for fornecido
+3. ✅ Retorna 400 se o cpf do cliente não for fornecido
+4. ✅ Retorna 400 se a senha do cliente não for fornecida
+5. ✅ Retorna 400 se a senha e confirmação senha do cliente forem diferentes
+6. ⛔ Retorna 400 se o email do cliente já existir
+7. ⛔ Retorna 400 se o cpf do cliente já existir
+8. ⛔ Retorna 500 se houver alguma falha na hora de salvar os dados
 
 
 ## Objeto Client
@@ -22,6 +23,7 @@
     name: string
     email: string
     password: string
+    repeatPassword:string
     cpf: string
     createdAt: Date
     updatedAt: Date
--- src/domain/usecases/client/create-client.usecase.spec.ts
+++ src/domain/usecases/client/create-client.usecase.spec.ts
@@ -4,6 +4,7 @@ import { ISchemaValidator } from '@/ports/validators/schema-validator.port'
 import { mock } from 'jest-mock-extended'
 import { IUUIDGenerator } from '@/ports/usecases/uuid/uuid-generator.port'
 import { IClientRepository } from '@/ports/repositories/client.port'
+import { InvalidParamError } from '@/shared/errors'
 
 const schemaValidator = mock<ISchemaValidator>()
 const uuidGenerator = mock<IUUIDGenerator>()
@@ -32,5 +33,46 @@ describe('CreateClientUseCase', () => {
     await sut.execute(input)
     expect(schemaValidator.validate).toHaveBeenCalledWith({ schema: 'clientSchema', data: input })
     expect(schemaValidator.validate).toHaveBeenCalledTimes(1)
+  })
+
+  test('should throws if name is empty', async () => {
+    input.name = ''
+    schemaValidator.validate.mockReturnValueOnce({ value: input, error: 'anyError' })
+    const output = sut.execute(input)
+    await expect(output).rejects.toThrow(new InvalidParamError('anyError'))
+  })
+
+  test('should throws if email is empty', async () => {
+    input.email = ''
+    schemaValidator.validate.mockReturnValueOnce({ value: input, error: 'anyError' })
+    const output = sut.execute(input)
+    await expect(output).rejects.toThrow(new InvalidParamError('anyError'))
+  })
+
+  test('should throws if cpf is empty', async () => {
+    input.cpf = ''
+    schemaValidator.validate.mockReturnValueOnce({ value: input, error: 'anyError' })
+    const output = sut.execute(input)
+    await expect(output).rejects.toThrow(new InvalidParamError('anyError'))
+  })
+
+  test('should throws if password is empty', async () => {
+    input.password = ''
+    schemaValidator.validate.mockReturnValueOnce({ value: input, error: 'anyError' })
+    const output = sut.execute(input)
+    await expect(output).rejects.toThrow(new InvalidParamError('anyError'))
+  })
+
+  test('should throws if repeatPassword is empty', async () => {
+    input.repeatPassword = ''
+    schemaValidator.validate.mockReturnValueOnce({ value: input, error: 'anyError' })
+    const output = sut.execute(input)
+    await expect(output).rejects.toThrow(new InvalidParamError('anyError'))
+  })
+
+  test('should throws if password and repeatPassword are different ', async () => {
+    schemaValidator.validate.mockReturnValueOnce({ value: input, error: 'anyError' })
+    const output = sut.execute(input)
+    await expect(output).rejects.toThrow(new InvalidParamError('anyError'))
   })
 })
