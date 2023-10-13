c3e6452495fa827f1019affceda6bd015299e18f
Rafael Silva <rafaelsilvamap@gmail.com>
2023-10-13 17:49:56 -0300

    feat: creted bcrypt adapter

---
+++ src/infra/adapters/encrypt/bcrypt.adapter.spec.ts
@@ -0,0 +1,22 @@
+import { IEncrypt } from '@/ports/usecases/encrypt/encrypt.port'
+import { BcryptAdapter } from './bcrypt.adapter'
+import bcrypt from 'bcrypt'
+
+describe('BcryptAdapter', () => {
+  let sut: IEncrypt
+
+  beforeEach(() => {
+    sut = new BcryptAdapter()
+  })
+
+  test('should return hash once input value', () => {
+    const hash = sut.encrypt('anyValue')
+    expect(hash).toBeTruthy()
+  })
+
+  test('should returna correctly hash', () => {
+    const hash = sut.encrypt('anyValue')
+    const hashCompare = bcrypt.compareSync('anyValue', hash)
+    expect(hashCompare).toBeTruthy()
+  })
+})
