892461872786cd657128068aca8655b5d85c1f84
Rafael Silva <rafaelsilvamap@gmail.com>
2023-10-14 14:44:36 -0300

    feat: created CreateClientUseCase and CreateClientController factories

--- src/infra/routes.ts
+++ src/infra/routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express'
 import { expressAdapter } from './adapters/http/express.adapter'
 import { makeHealthcheckController } from './factories/controllers/healthcheck-controller.factory'
 import { makeCreateOrderController } from './factories/controllers/create-order-controller.factory'
+import { makeCreateClientController } from './factories/controllers/create-client-controller.factory'
 
 const router = Router()
 
 router.get('/healthcheck', expressAdapter(makeHealthcheckController()))
 router.post('/orders', expressAdapter(makeCreateOrderController()))
+router.post('/client', expressAdapter(makeCreateClientController()))
 
 export { router }
