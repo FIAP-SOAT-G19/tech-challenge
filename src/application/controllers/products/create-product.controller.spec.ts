bdcd0e40281e024c9a6cf957dac0a18a80bfa39e
Vanessa <vanessalima.f@hotmail.com>
2023-10-14 19:04:33 -0300

    test: create product controller unit test

---
+++ src/application/controllers/products/create-product.controller.spec.ts
@@ -0,0 +1,122 @@
+import { HttpRequest } from '@/shared/types/http.types'
+import { mock } from 'jest-mock-extended'
+import { serverError } from '@/shared/helpers/http.helper'
+import { ICreateProductUseCase } from '@/ports/usecases/product/create-product.port'
+import { CreateProductController } from './create-product.controller'
+
+const createProductUseCase = mock<ICreateProductUseCase>()
+
+describe('CreateProductController', () => {
+  let createProductController: CreateProductController
+  let input: HttpRequest
+
+  beforeAll(() => {
+    createProductController = new CreateProductController(createProductUseCase)
+    createProductUseCase.execute.mockResolvedValue('productId')
+  })
+
+  beforeEach(() => {
+    input = {
+      body: {
+        name: 'Coca Cola',
+        category: 'drink',
+        price: 6,
+        description: 'description',
+        image: 'url'
+      }
+    }
+  })
+  describe('POST /products', () => {
+    test('should execute CreateProductUseCase once and with valid input', async () => {
+      await createProductController.execute(input)
+
+      expect(createProductUseCase.execute).toHaveBeenCalledTimes(1)
+      expect(createProductUseCase.execute).toHaveBeenCalledWith(input.body)
+    })
+
+    test('should return a productId on success', async () => {
+      const output = await createProductController.execute(input)
+
+      expect(output).toEqual({
+        statusCode: 201,
+        body: { productId: 'productId' }
+      })
+    })
+
+    test('should return a properly missing name error', async () => {
+      input.body.name = ''
+      const output = await createProductController.execute(input)
+
+      expect(output).toEqual({
+        statusCode: 400,
+        body: {
+          error: 'MissingParamError',
+          message: 'Missing param: product name'
+        }
+      })
+    })
+
+    test('should return a properly missing category error', async () => {
+      input.body.category = ''
+      const output = await createProductController.execute(input)
+
+      expect(output).toEqual({
+        statusCode: 400,
+        body: {
+          error: 'MissingParamError',
+          message: 'Missing param: product category'
+        }
+      })
+    })
+
+    test('should return a properly missing price error', async () => {
+      input.body.price = null
+      const output = await createProductController.execute(input)
+
+      expect(output).toEqual({
+        statusCode: 400,
+        body: {
+          error: 'MissingParamError',
+          message: 'Missing param: product price'
+        }
+      })
+    })
+
+    test('should return a properly missing description error', async () => {
+      input.body.description = null
+      const output = await createProductController.execute(input)
+
+      expect(output).toEqual({
+        statusCode: 400,
+        body: {
+          error: 'MissingParamError',
+          message: 'Missing param: product description'
+        }
+      })
+    })
+
+    test('should return a properly missing image error', async () => {
+      input.body.image = ''
+      const output = await createProductController.execute(input)
+
+      expect(output).toEqual({
+        statusCode: 400,
+        body: {
+          error: 'MissingParamError',
+          message: 'Missing param: product image'
+        }
+      })
+    })
+
+    test('should return an error if CreateOrderUseCase throws error', async () => {
+      const error = new Error('Internal server error')
+      createProductUseCase.execute.mockImplementationOnce(() => {
+        throw error
+      })
+
+      const output = await createProductController.execute(input)
+
+      expect(output).toEqual(serverError(error))
+    })
+  })
+})
