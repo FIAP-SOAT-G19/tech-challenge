b24db3d29b0c7c34d1d0126b624c68e645d1984e
Filipe Siqueira <filipedev040990@gmail.com>
2023-10-16 13:34:05 -0300

    refactor: add validation of delete order usecase

--- src/domain/usecases/order/delete-order.usecase.ts
+++ src/domain/usecases/order/delete-order.usecase.ts
@@ -1,12 +1,27 @@
 import { IOrderRepository } from '@/ports'
 import { IDeleteOrderUseCase } from '@/ports/usecases/order/delete-order.port'
+import constants from '@/shared/constants'
+import { InvalidParamError, MissingParamError } from '@/shared/errors'
 
 export class DeleteOrderUseCase implements IDeleteOrderUseCase {
   constructor(private readonly orderRepository: IOrderRepository) {}
   async execute (orderNumber: string): Promise<void> {
+    await this.validate(orderNumber)
+    await this.orderRepository.delete(orderNumber)
+  }
+
+  private async validate (orderNumber: string): Promise<void> {
+    if (!orderNumber) {
+      throw new MissingParamError('orderNumber')
+    }
+
     const order = await this.orderRepository.getByOrderNumber(orderNumber)
-    if (order) {
-      await this.orderRepository.delete(orderNumber)
+    if (!order) {
+      throw new InvalidParamError('orderNumber')
+    }
+
+    if (order.status !== constants.ORDER_STATUS.CANCELED) {
+      throw new InvalidParamError('status')
     }
   }
 }
