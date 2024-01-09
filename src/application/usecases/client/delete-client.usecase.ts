66cb80bac2ea709d382b93e21ec449a39bf8e684
Rafael Silva <rafaelsilvamap@gmail.com>
2024-01-09 17:57:43 -0300

    feat: created DeleteClientGateway

--- src/application/usecases/client/delete-client.usecase.ts
+++ src/application/usecases/client/delete-client.usecase.ts
@@ -1,17 +1,17 @@
-import { IClientRepository } from '@/application/interfaces'
+import { IDeleteClientGateway } from '@/application/interfaces/gateways/client/delete-client-gateway.interface'
 import { IDeleteClientUseCase } from '@/application/interfaces/usecases/client/delete-client.interface'
 import { ClientNotFoundError } from '@/infra/shared'
 
 export class DeleteClientUseCase implements IDeleteClientUseCase {
-  constructor(private readonly clientRepository: IClientRepository) { }
+  constructor(private readonly gateway: IDeleteClientGateway) { }
 
   async execute(input: IDeleteClientUseCase.Input): Promise<void> {
     await this.validate(input)
-    await this.clientRepository.delete(input.id)
+    await this.gateway.deleteClient(input.id)
   }
 
   private async validate(input: IDeleteClientUseCase.Input): Promise<void> {
-    const client = await this.clientRepository.getById(input.id)
+    const client = await this.gateway.getClientById(input.id)
     if (!client) throw new ClientNotFoundError()
   }
 }
