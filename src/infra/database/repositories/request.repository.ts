f4fbbe5172075fccfd6ee2e26bb75efe7fd15a35
Filipe Siqueira <filipedev040990@gmail.com>
2024-01-22 20:42:56 -0300

    refactor: limit logs of requests by url

--- src/infra/database/repositories/request.repository.ts
+++ src/infra/database/repositories/request.repository.ts
@@ -1,24 +1,8 @@
-import { CreateRequestRepositoryInput, RequestRepositoryInterface, UpdateRequestRepositotyInput } from '@/application/interfaces'
+import { CreateRequestRepositoryInput, RequestRepositoryInterface } from '@/application/interfaces'
 import { prismaClient } from '../prisma-client'
 
 export class RequestsRepository implements RequestRepositoryInterface {
-  async create (input: CreateRequestRepositoryInput): Promise<string> {
-    const request = await prismaClient.request.create({ data: input })
-    return request.id
-  }
-
-  async update (input: UpdateRequestRepositotyInput): Promise<void> {
-    const data: Omit<UpdateRequestRepositotyInput, 'requestId'> = {
-      status: input.status,
-      output: input.output,
-      updatedAt: input.updatedAt
-    }
-
-    await prismaClient.request.update({
-      data,
-      where: {
-        id: input.requestId
-      }
-    })
+  async create (data: CreateRequestRepositoryInput): Promise<void> {
+    await prismaClient.request.create({ data })
   }
 }
